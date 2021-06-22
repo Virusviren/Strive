8f4b472964b6f9aa2bf088236ddfce133caa3b15
Viren <virenpatil1@outlook.com>
2021-06-22 09:14:33 +0200

    New Start

--- M4-D2/strive-restaurant/src/LatestRelease.jsx
+++ M4-D2/strive-restaurant/src/LatestRelease.jsx
@@ -1,31 +1,52 @@
 import React from "react";
 import books from "../src/data/history.json";
 import Carousel from "react-bootstrap/Carousel";
-export default function LatestRelease() {
-  return (
-    <div className="container mb-5 p-2">
-      <Carousel>
-        {books.map((book) => (
-          <Carousel.Item className="img-fluid ">
-            <div className="d-flex justify-content-center">
-              <img
-                className="d-block w-25  "
-                src={book.img}
-                alt="First slide"
-              />
-            </div>
 
-            <Carousel.Caption>
-              <div className="info  text-nowrap">
-                <h3 className="text-dark">First slide label</h3>
-                <p className="text-dark">
-                  vitae elit libero, a pharetra augue mollis interdum.
-                </p>
+function showPopup() {
+  return <textarea placeholder="hello"></textarea>;
+}
+
+export default class LatestRelease extends React.Component {
+  state = {
+    selected: null,
+  };
+  render() {
+    return (
+      <div className="container mb-5 p-2">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          {books.map((book) => (
+            <div className="col">
+              <div className="card shadow-sm">
+                <img src={book.img} style={{ height: "50%" }} />
+
+                <div className="card-body">
+                  <p className="card-text">{book.title}</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                        onClick={() => this.setState({ selected: book.asin })}
+                      >
+                        Add Comment
+                      </button>
+                      <button
+                        onClick={showPopup}
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        Delete Comment
+                      </button>
+                    </div>
+                    <small className="text-muted">Price {book.price}$</small>
+                  </div>
+                </div>
               </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    </div>
-  );
+            </div>
+          ))}
+        </div>
+        {/* {this.state.selected && <CmmentArea asin={this.state.selected}/>} */}
+      </div>
+    );
+  }
 }
